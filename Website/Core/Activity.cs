12cbd8655d06a7c3d8b685e59d0cdca777e43eaf
amirmatz <ramatz777@gmail.com>
2015-11-19 18:10:03 +0200

    added core

---
+++ Website/Core/Activity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class Activity
+    {
+        public string Id { get; set; }
+
+        public string User { get; set; }
+
+        public string Message { get; set; }
+    }
+}
---
+++ Website/Core/Protest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class Protest
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string About { get; set; }
+
+        public IEnumerable<string> Images { get; set; }
+
+        public IEnumerable<string> Links { get; set; }
+
+        public IEnumerable<Activity> Activities { get; set; }
+    }
+}
