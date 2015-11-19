5c484b515a05a0fc96f0aac15f5babc3b5f3d1b8
amirmatz <ramatz777@gmail.com>
2015-11-19 23:45:39 +0200

    users functions ready

--- Website/Core/BasicUserSettings.cs
+++ Website/Core/BasicUserSettings.cs
@@ -8,13 +8,10 @@ namespace Core
 {
     public class BasicUserSettings
     {
-        public string Name { get; set; }
-
         public string Token { get; set; }
 
         public BasicUserSettings()
         {
-            Name = Name ?? string.Empty;
             Token = Token ?? string.Empty;
         }
     }
