356fffea6c8d44170bebba587f1f4adcdb04c78e
amirmatz <ramatz777@gmail.com>
2015-11-19 19:11:48 +0200

    added getProtestById

--- Website/Core/Protest.cs
+++ Website/Core/Protest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,8 @@ namespace Core
 {
     public class Protest
     {
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonId]
         public string Id { get; set; }
 
         public string Title { get; set; }
---
+++ Website/Website/Controllers/ProtestsController.cs
@@ -0,0 +1,27 @@
+﻿using Core;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Website.Controllers
+{
+    public class ProtestsController : ApiController
+    {
+        public Protest Get(string id)
+        {
+            try
+            {
+                return DataManager.Instance.GetProtestById(id);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("ha ha");
+                return null;
+            }
+        }
+    }
+}
