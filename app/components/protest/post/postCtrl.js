c0aaf58bee2a74776f2e71819b8d465701631282
Barle <Barlevy63@gmail.com>
2015-11-20 07:15:32 +0200

    adding post option

---
+++ app/components/protest/post/postCtrl.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('proProtest')
+        .controller('PostCtrl', PostCtrl);
+
+    PostCtrl.$inject = ['$scope', '$stateParams', '$state', 'ProtestService'];
+
+    /* @ngInject */
+    function PostCtrl($scope, $stateParams, $state, ProtestService) {
+        var vm = this;
+        vm.title = 'PostCtrl';
+
+        vm.post = post;
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            var protestId = $stateParams.id;
+            ProtestService.getProtest(protestId).then(function (protest) {
+                vm.protest = protest;
+            })
+        }
+
+        function post() {
+            ProtestService.sendPost(vm.message, vm.protest.id)
+            $state.go('protest', { id: vm.protest.id });
+        }
+
+    }
+
+})();
